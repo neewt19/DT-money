a4b400f52b9003a35836ad99e240df44bc8cc94f
neewt19 <newthonaraujo19@gmail.com>
2024-01-18 11:21:54 -0300

    contexto e calculando transação

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,29 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import * as S from './styles'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
 
 export function Summary(){
+    const { transactions } = useContext(TransactionsContext)
+
+    //{income:0, outcome:0, total:0}
+    const sumary = transactions.reduce(
+    (acc, transaction) => {
+        if(transaction.type == 'income'){
+            acc.income += transaction.price
+            acc.total += transaction.price
+        }else {
+            acc.outcome += transaction.price
+            acc.total -= transaction.price
+        }
+        return acc
+    }, 
+    {
+        income:0,
+        outcome:0, 
+        total:0
+    }
+    )
     return (
         <S.SumaryContainer>
             <S.SummaryCard>
@@ -9,7 +31,7 @@ export function Summary(){
                     <span>Entradas</span>
                     <ArrowCircleUp size={32} color="#00b37e"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{sumary.income}</strong>
             </S.SummaryCard>  
 
             <S.SummaryCard>
@@ -17,7 +39,7 @@ export function Summary(){
                     <span>Saídas</span>
                     <ArrowCircleDown size={32} color="#f75a68"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{sumary.outcome}</strong>
             </S.SummaryCard>
 
             <S.SummaryCard variant='green'>
@@ -25,7 +47,7 @@ export function Summary(){
                     <span>Total</span>
                     <CurrencyDollar size={32} color="#fff"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{sumary.total}</strong>
             </S.SummaryCard>
         </S.SumaryContainer>
     )
